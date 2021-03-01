f389e0dcee82b4b05e373f6835727b5af3242b59
patrykgitt <patrykbrn@gmail.com>
2021-03-01 19:58:00 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { TodoList } from "./todoList";
-import { TodoItem } from "./todoItem";
+import { Alarm } from "./alarm";
+import { AlarmMgnt } from "./alarmMgnt";
 
 @Component({
   selector: 'app-root',
@@ -8,22 +8,22 @@ import { TodoItem } from "./todoItem";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private list = new TodoList("Karo", [
-    new TodoItem("Go for run", true),
-    new TodoItem("Get flowers"),
-    new TodoItem("Collect tickets"),
+  private list = new AlarmMgnt("Alarm", [
+    new Alarm("J6 axis overspeed", true, true),
+    new Alarm("Undervoltage error", true),
+    new Alarm("Communication error"),
   ]);  
   
   get username(): string {
-    return this.list.user;
+    return this.list.listName;
   }
 
   get itemCount(): number {
     return this.items.length;
   }
 
-  get items(): readonly TodoItem[] {
-    return this.list.items.filter(item => this.showComplete || !item.complete);
+  get items(): readonly Alarm[] {
+    return this.list.items.filter(item => this.showApproved || !item.approved);
   }
 
   addItem(newItem) {
@@ -32,5 +32,5 @@ export class AppComponent {
     }
   }
 
-  showComplete: boolean = false;
+  showApproved: boolean = false;
 }
